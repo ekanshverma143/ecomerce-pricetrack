3ae84422d55474c26d585aca42836d3c91a7b01e
Ekansh Verma <21bee083@nith.ac.in>
2024-06-30 12:31:47 +0530

    project setup and navbar added

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import Navbar from "@/components/Navbar";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const space_Grotesk = Space_Grotesk({ 
+  subsets: ["latin"],
+   weight:['300','400','500','600','700']
+ });
+
+export const metadata: Metadata = {
+  title: "PriceTrack",
+  description: "Created by Ekansh Verma",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10x1 mx-auto">
+          <Navbar/>
+          {children}
+        </main>
+        
+        </body>
+    </html>
+  );
+}
